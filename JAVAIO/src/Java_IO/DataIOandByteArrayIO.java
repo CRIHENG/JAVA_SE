b841ad9caca4cf2ccb95471ed2334c76554895b5
mr.c <crh1028vip@163.com>
2017-11-21 21:46:43 +0800

    数据流和字节数组流 处理基本类型 如 int double...

---
+++ JAVAIO/src/Java_IO/DataIOandByteArrayIO.java
@@ -0,0 +1,16 @@
+package Java_IO;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class DataIOandByteArrayIO {
+    @Test
+    public void testDataIoAndByteArrayIO(){
+
+
+        LocalDate L =LocalDate.now();
+        System.out.print(L);
+
+    }
+}
