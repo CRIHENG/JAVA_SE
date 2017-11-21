48d0d4129bde6bf0a4aaaf384237e7c71f68d2d9
mr.c <crh1028vip@163.com>
2017-11-21 22:56:14 +0800

    数据流和字节数组流 处理基本类型 如 int double...

--- JAVAIO/src/Java_IO/DataIOandByteArrayIO.java
+++ JAVAIO/src/Java_IO/DataIOandByteArrayIO.java
@@ -2,15 +2,30 @@ package Java_IO;
 
 import org.junit.Test;
 
-import java.time.LocalDate;
+import java.io.*;
+
 
 public class DataIOandByteArrayIO {
     @Test
     public void testDataIoAndByteArrayIO(){
 
+        ByteArrayOutputStream baos=new ByteArrayOutputStream();
+        DataOutputStream  dos=new DataOutputStream(baos);
+        try {
+            dos.writeDouble(Math.random());
+            dos.writeBoolean(true);
+            ByteArrayInputStream bais=new ByteArrayInputStream(baos.toByteArray());
+            DataInputStream    dis=new DataInputStream(bais);
+            //先写先读
+            System.out.print(dis.readDouble());
+            System.out.print(dis.readBoolean());
+            dos.close(); dis.close();
 
-        LocalDate L =LocalDate.now();
-        System.out.print(L);
+        }catch (IOException e){
+            e.printStackTrace();
+
+        }
+
 
     }
 }
