90d8853d34eb80caf52fd5e9bb57cd27f4abe1e4
mr.c <crh1028vip@163.com>
2017-11-27 00:22:20 +0800

    数据流和字节数组流 处理基本类型 如 int double...

--- JAVAIO/src/Java8/LearnOptional.java
+++ JAVAIO/src/Java8/LearnOptional.java
@@ -8,18 +8,45 @@ public class LearnOptional {
 /*
 *Optional容器类的常用方法:
 * Opteional.of(T t):创建一个Optional 实例
-* Optional.empty:创建一个空的Optional实例
-* Optional.ofNullable:可以简单的理解为包含了上述2个方法;
+* Optional.empty():创建一个空的Optional实例
+* Optional.ofNullable(T t):可以简单的理解为包含了上述2个方法;
+* isPreSend()判断是否有值
+* orElse(T t)判断是否有值，没有则替换
+* orElseGet(Suppler s) 判断你是否有值，没有就返回定义的对象返回的值或者已有的对象返回的值
+* map();判断是否有值，处理后返回Optional 否则返回Optional.empty();
+* flatMap():和MAP类似 但是参数一定要求返回的时Optional类型;
 * */
 
     @Test
-    public void testOfNullable() {
-        Optional<String> opNotNull=Optional.ofNullable(new String("测试ofNullable"));
-        System.out.println(opNotNull.get());
-        Optional<String> op=Optional.ofNullable(null);
-        op.get();
+    public void testMap() {
+        Optional<String> opMap = Optional.ofNullable(new String("123456"));
 
+        Optional<Integer> strToInt= opMap.map((e)-> Integer.parseInt(e));
+
+         System.out.println(strToInt.get());
+
+         Optional<Double> strToDouble =opMap.flatMap((e)->Optional.of(Double.parseDouble(e)));
+         System.out.println(strToDouble.get());
     }
+
+    @Test
+    public void testOfNullable() {
+        Optional<String> opNotNull = Optional.ofNullable(new String("测试ofNullable"));
+        System.out.println(opNotNull.get());
+        Optional<String> op = Optional.ofNullable(null);
+        if (op.isPresent()) {
+            op.get();
+        }
+        String str = op.orElse("没有值，那个人走了让我告诉你下,你该忙什么就忙什么吧");
+        System.out.println(str);
+        //自定义对象
+        String str01 = op.orElseGet(() -> {
+            return new String("哎呀，这叫函数式编程啊！");
+        });
+        System.out.println(str01);
+    }
+
+    ;
 
 
     @Test
