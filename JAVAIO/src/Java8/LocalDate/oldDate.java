67d4bfb3abc74726ba557c1a768318695063712a
mr.c <crh1028vip@163.com>
2017-11-29 22:42:47 +0800

    数据流和字节数组流 处理基本类型 如 int double...

---
+++ JAVAIO/src/Java8/LocalDate/oldDate.java
@@ -0,0 +1,17 @@
+package Java8.LocalDate;
+
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+
+public class oldDate {
+
+    @Test
+    public void testSimpleDateFormat(){
+
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyyDDmm");
+
+
+
+    }
+}
